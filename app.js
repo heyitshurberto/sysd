60f9c1fb53c965a0386a1d4af5b2a4e52c6c90ae
heyitshurberto <hubkiree@outlook.com>
2026-01-24 22:15:05 +0530

    update

--- app.js
+++ app.js
@@ -2572,7 +2572,7 @@ const renderLoginPage = () => `
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CARLUCCI CAPITAL Portal</title>
+  <title>Secure Access Portal</title>
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
     * { margin: 0; padding: 0; box-sizing: border-box; }
